cab820d02cc29600931299069805a8e7ed71cc17
bijans655 <bijans655@gmail.com>
2025-11-04 13:13:09 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,18 +4,3 @@ export default {
     autoprefixer: {},
   },
 }
-```
-Click **"Commit changes"**
-
----
-
-### **File 6: `.gitignore`**
-**"Add file"** → **"Create new file"**
-Name: `.gitignore`
-```
-node_modules
-dist
-.env
-.env.local
-.DS_Store
-.vercel
