ef5a37f86a24437858860eabff10dcd2d7e4d5ff
bijans655 <bijans655@gmail.com>
2025-11-04 12:44:50 +0530

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+Click **"Commit changes"**
+
+---
+
+### **File 6: `.gitignore`**
+**"Add file"** → **"Create new file"**
+Name: `.gitignore`
+```
+node_modules
+dist
+.env
+.env.local
+.DS_Store
+.vercel
